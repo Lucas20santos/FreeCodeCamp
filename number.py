6e9790d55acce1b30688f0379559152b40be790c
lucas de souza santos <lucass.santoss210@gmail.com>
2022-07-22 22:27:39 -0300

    push number python

---
+++ number.py
@@ -0,0 +1,21 @@
+from math import *
+
+number = 79
+print(78)
+print(number)
+
+# Operação
+print(22 + 34)
+print(22 + 33.22)
+print(22 - 11)
+print(22 * 10)
+print(22 / 2)
+print(22 % 5)
+print(abs(-5))
+print(max(3, 4, 5))
+print(min(1, 2, 3))
+print(round(3.5))
+
+print(sqrt(100))
+
+
