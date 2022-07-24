785fb916c22929629973db273741f37d50a563e6
lucas de souza santos <lucass.santoss210@gmail.com>
2022-07-23 22:48:54 -0300

    the end of list content

---
+++ list.py
@@ -0,0 +1,16 @@
+countries = ['United Kingdom', ' Ghana', 'Nigeria', 'Austrilia', 'New Zealand']
+
+print(countries)
+print(countries[0])
+print(countries[1])
+print(countries[2])
+print(countries[3])
+
+print(countries[1:])
+print(countries[1:3])
+
+print(type(countries))
+
+print(countries[-1])
+
+print(len(countries))
