412fe4d8abcdb03403f8ca3957e6585acbd3b472
Lucas de Souza Santos <lucas.santoss210@gmail.com>
2023-01-04 02:07:32 -0300

    Subindo primeira tarefa

---
+++ JavaScript/index.js
@@ -0,0 +1,21 @@
+var counterVal = parseInt(document.getElementById("count-el").innerHTML);
+   
+function incrementar() 
+{
+     counterVal++
+    if(counterVal > 10)
+    {
+        counterVal = 0
+    }
+    document.getElementById("count-el").innerHTML = counterVal.toString()
+}
+
+function decremento() {
+    counterVal--;
+    if(counterVal < 0)
+    {
+        counterVal = 10;
+    }
+    document.getElementById("count-el").innerHTML = counterVal;
+}
+    
